094d0c0343f50278bd064a1d25c30c41cfd270d4
rdorin <rdorin@palantir.com>
2020-05-08 23:30:25 +0100

    adding database and movie support

--- application/movie_lottery.py
+++ application/movie_lottery.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from random import randint
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+# Setting up movie table
+class Movie(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	movie_name = db.Column(db.String(50))
 
 @app.route('/')
 def index():
@@ -14,4 +26,15 @@ def render_form(name=None):
 def add_movie():
 	if request.method == 'POST':
 		mname = request.form.get('mname','')
-		return mname
+		movie = Movie(movie_name=mname)
+		db.session.add(movie)
+		db.session.commit()
+
+	return "<h1>Added movie to the database!</h1>"
+
+@app.route('/movie/lottery')
+def get_movie():
+	count = db.session.query(Movie).count()
+	id = randint(1,count)
+	movie = Movie.query.filter_by(id=id).first()
+	return f"<h1>{ movie.movie_name }</h1>"
